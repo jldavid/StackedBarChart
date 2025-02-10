04b6276ab7bac37c9cc7fcd5d79323fd2026ca1f
jldavid <jldavid.mail@gmail.com>
2025-02-09 23:22:22 -0500

    First commit

---
+++ StackedBarChart/StackedBarChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  StackedBarChartApp.swift
+//  StackedBarChart
+//
+//  Created by JL David on 2025-02-05.
+//
+
+import SwiftUI
+
+@main
+struct StackedBarChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
